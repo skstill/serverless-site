f6f0e4ba979639d3b8acdb21acb45431a2943a11
skstill <30243468+skstill@users.noreply.github.com>
2021-06-03 13:52:56 -0600

    different buttons

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,7 +1,9 @@
 const fetch = require('node-fetch')
 
-exports.handler = async function () {
+exports.handler = async function (event, context) {
     const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+
+console.log(event)
 
     const response = await fetch(POKE_API)
     const data = await response.json()
